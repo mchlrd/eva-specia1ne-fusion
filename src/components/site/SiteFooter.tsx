3c4062f3b8913fc20a5cd51010b8fa5f8de981ea
Michael Rud <159171871+mchlrd@users.noreply.github.com>
2026-08-24 22:25:27 -0400

    Update footer logo to transparent version

--- src/components/site/SiteFooter.tsx
+++ src/components/site/SiteFooter.tsx
@@ -1,7 +1,7 @@
 import { Link } from "@tanstack/react-router";
 
 import { company, nav } from "./data";
-import logo from "@/assets/evarotech-logo.png";
+import logo from "@/assets/evarotech-logo-transparent.png";
 
 export function SiteFooter() {
   return (
